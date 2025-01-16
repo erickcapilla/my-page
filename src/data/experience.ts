1d0e087904c54b197e53005ee3b7647a1244499b
Erick Capilla <erickpoblano25@gmail.com>
2025-01-16 16:02:44 -0600

    Add experience

---
+++ src/data/experience.ts
@@ -0,0 +1,11 @@
+import type { Experience } from "@/types";
+
+export const experience: Experience[] = [
+  {
+    time: "2024",
+    rol: "Frontend Developer",
+    company: "Gleted web",
+    url: "https://www.gleted-web.com",
+    description: "Development of a project from start to finish on the frontend side, creating modules for residential management using React and TypeScript. Deploying the backend on a Linux server, connecting the API to the frontend, and deploying the project to production."
+  }
+]
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export type Experience = {
+  time: string;
+  rol: string;
+  company: string;
+  url: string;
+  description: string;
+}
