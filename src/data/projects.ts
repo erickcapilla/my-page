3bc5cb539bed90308a3a58b73393d4847a490be6
Erick Capilla <erickpoblano25@gmail.com>
2025-01-16 13:03:00 -0600

    Update UI

---
+++ src/data/projects.ts
@@ -0,0 +1,18 @@
+export const projects = [
+  {
+    title: "Graduation Process Management System of FCBIyT (SIGET)",
+    Description:
+      "A system to improve the graduation process at the Faculty of Basic Sciences, Engineering, and Technology (FCBIyT). It optimizes delivery times, reduces paper usage, and enhances the overall process experience.",
+    img: "/Imagen2.png",
+    url: "https://github.com/erickcapilla/siget",
+    technologies: ["React", "Tailwind", "TypeScript"],
+  },
+  {
+    title: "Product Catalog (XCELENT)",
+    Description:
+      "A product catalog designed to improve the sale of products that were previously sold through WhatsApp. Features include PayPal payments, shipment tracking, and product browsing without the need for an account.",
+    img: "/Imagen1.png",
+    url: "https://github.com/erickcapilla/xcelente",
+    technologies: ["React", "Tailwind", "Firebase", "PayPal", "TypeScript"],
+  },
+];
---
+++ src/data/technologies.ts
@@ -0,0 +1,50 @@
+import {
+  Reactts,
+  Js,
+  TypeScript,
+  Node,
+  Git,
+  Tailwind,
+  Firebase,
+  Astro,
+  Docker,
+} from "@react-symbols/icons";
+
+export const technologies = [
+  {
+    name: "React",
+    icon: Reactts,
+  },
+  {
+    name: "JavaScript",
+    icon: Js,
+  },
+  {
+    name: "TypeScript",
+    icon: TypeScript,
+  },
+  {
+    name: "Node.js",
+    icon: Node,
+  },
+  {
+    name: "Git",
+    icon: Git,
+  },
+  {
+    name: "Tailwind",
+    icon: Tailwind,
+  },
+  {
+    name: "Firebase",
+    icon: Firebase,
+  },
+  {
+    name: "Astro",
+    icon: Astro,
+  },
+  {
+    name: "Docker",
+    icon: Docker,
+  },
+];
